8c1a0d00c4613b23da2de5bafb2e675bbab4d549
Michał Szmanda <32749003+szmanda@users.noreply.github.com>
2018-12-05 22:55:23 +0100

    Update punkt.cpp

--- 3_Practise/punkt.cpp
+++ 3_Practise/punkt.cpp
@@ -1,16 +1,34 @@
-﻿#include "punkt.h"
+#include "punkt.h"
 #include <iostream>
 
 using namespace std;
 
 Punkt::Punkt(float xx, float yy){
+    cout<<"Konstruktor(float,float)\n";
     x = xx;
     y = yy;
 }
-Punkt::Przesun(float xx, float yy){
+void Punkt::Przesun(float xx, float yy){
+    cout<<"przesun(float,float)\n";
     x += xx;
     y += yy;
 }
-Punkt::Wyswietl(){
+/*void Punkt::Wyswietl(){
     cout<<"\n\tx = "<<x<<"\ty = "<<y<<endl;
+}*/
+float Punkt::Odcieta(){
+    cout<<"Odcieta()\n";
+    return x;
+}
+float Punkt::Rzedna(){
+    cout<<"Rzedna()\n";
+    return y;
+}
+void Punkt::Skaluj(float skala){
+    cout<<"Skaluj()\n";
+    x *= skala;
+    y *= skala;
+}
+void Punkt::Obracaj(float kat){
+
 }
