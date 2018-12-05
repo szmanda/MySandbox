9b3bc29a076a36e418cd05250b201b1a98d548cc
Michał Szmanda <32749003+szmanda@users.noreply.github.com>
2018-12-05 22:54:47 +0100

    Update 3_Practise.cpp

--- 3_Practise/3_Practise.cpp
+++ 3_Practise/3_Practise.cpp
@@ -1,10 +1,11 @@
-﻿#include "punkt.h"
+#include "punkt.h"
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
+    cout<<"---\nZadanie 3.1"<<endl;
     /*
     Zadanie 3.1:
     Zrealizować klasę punkt do obsługi punktów na płaszczyźnie. Klasa ma zawierać:
@@ -17,15 +18,38 @@ int main()
         - plik źródłowy z deklaracją klasy;
         - plik śródłowy zawierający jej definicję
     */
-    cout<<"---\nZadanie 3.1"<<endl;
     // zob. pliki punkt.h; punkt.cpp
     Punkt * pun = new Punkt(0,0);
 
-    pun->Wyswietl();
     pun->Przesun(5,-2);
-    pun->Wyswietl();
     pun->Przesun(-12,4);
-    pun->Wyswietl();
+    //pun->Wyswietl();
+
+    cout<<"---\nZadanie 3.2"<<endl;
+    /*
+    Zadanie 3.2
+    Zmodyfikować klasę punkt, nie zawierającą funkcji Wyswietl, a zamiast niej aby zachować hermetyzację danych
+    napisać w zamian dwie funkcjie Odcieta i Rzedna zwracjające odpowiednio odciętą i rzędną punktu.
+    Zaadoptowanwać program testowy z poprzedniego zadania
+    */
+
+    cout<<"\tx = "<<pun->Odcieta()<<endl;
+    cout<<"\tx = "<<pun->Rzedna()<<endl;
+    /*
+    Utworzenie funkcji dostępu (tutaj Ocieta i Rzedna) umożliwia wymuszenie zachowanie hermetyzacji danych.
+    Dzięki takim publiczmym funkcjom zwracających prywatne zmienne można zachować bezpieczeństwo przed nadpisaniem
+    oraz kontrolę nad tym w jaki sposób zmienna ma być dostępna poza klasą.
+    */
+
+    cout<<"---\nZadanie 3.3"<<endl;
+    /*
+    Zadanie 3.3
+    Dodać do poprzedniej klasy nowe funkcje skłądowe:
+        - Skaluj wykonującą skalowanie o podanym w argumencie współczynniku;
+        - Obracaj wykonującą obrót o kąt podany w argumencie;
+        - Promien i Teta obliczające współżędne punktu.
+    */
+    pun->Skaluj(4);
 
     delete pun;
 
